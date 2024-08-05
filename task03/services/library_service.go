b730c0790e75eb0318f950fbbd25cbebc5b515dc
poseidon2022 <kidusm82@gmail.com>
2024-08-05 17:16:59 +0300

    Interface defined

--- task03/services/library_service.go
+++ task03/services/library_service.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"task03/models"
+)
+
+type LibraryManaager interface {
+	AddBook(book models.Book)
+	RemoveBook(bookID int)
+	BorrowBook(bookID int , memberID int) error
+	ReturnBook(bookID int, memberID int) error
+	ListAvailableBooks() []models.Book
+	ListBorrowedBooks(memberID int) []models.Book
+}
