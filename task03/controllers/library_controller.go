0ee359fd48121da93708e4bb27dfe49f1b70ae4e
poseidon2022 <kidusm82@gmail.com>
2024-08-05 21:24:28 +0300

    implemented some functions

--- task03/controllers/library_controller.go
+++ task03/controllers/library_controller.go
@@ -2,4 +2,64 @@ package controllers
 
 import (
 	"task03/services"
+	"task03/models"
+	"strconv"
+	"os"
+	"bufio"
+	"fmt"
+	"strings"
 )
+
+
+
+func bookInput() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the ID of the book to be added: ")
+	book_id, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error while accepting the ID")
+		return
+	}
+
+	book_id = strings.TrimSpace(book_id)
+	book_id_num ,err := strconv.Atoi(book_id)
+	for err != nil {
+		fmt.Println("Please enter a valid id number")
+		fmt.Print("Enter the ID of the book to be added: ")
+		book_id, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error while accepting the ID")
+			return
+		}
+		book_id = strings.TrimSpace(book_id)
+		book_id_num , err = strconv.Atoi(book_id)
+	}
+
+	fmt.Print("Enter the Title of the book to be added: ")
+	title, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error while accepting the ID")
+		return
+	}
+
+	title = strings.TrimSpace(title)
+
+	fmt.Print("Enter the author of the book to be added: ")
+	author, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error while accepting the ID")
+		return
+	}
+
+	author = strings.TrimSpace(author)
+	
+	status := "Available"
+
+	newBook := models.Book{
+		ID : book_id_num, 
+		Title: title,
+		Author: author,
+		Status: status,
+	}
+	services.AddBook(newBook)
+}
